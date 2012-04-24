d6e9607ebb6b4e4ee942286f7c77e2981cbd3a2e
Matt Beckett <beck24@gmail.com>
2012-04-24 11:25:30 -0600

    Initial Commit

---
+++ au_bookmarks_widget/start.php
@@ -0,0 +1,6 @@
+<?php
+
+// no setup required here
+// all taken care of in the au_widgets_framework
+// only thing here is views for the widget
+// start.php is required for the plugin skeleton however.
---
+++ au_bookmarks_widget/views/default/widgets/bookmarks/content.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Elgg bookmarks widget
+ *
+ * @package Bookmarks
+ */
+$widget = $vars['entity'];
+
+// now we can get our content
+$options = eligo_get_display_entities_options($widget);
+$content = elgg_list_entities($options);
+
+
+echo $content;
+
+if ($content) {
+	$url = "bookmarks/owner/" . elgg_get_page_owner_entity()->username;
+	$more_link = elgg_view('output/url', array(
+		'href' => $url,
+		'text' => elgg_echo('bookmarks:more'),
+		'is_trusted' => true,
+	));
+	echo "<span class=\"elgg-widget-more\">$more_link</span>";
+} else {
+	echo elgg_echo('bookmarks:none');
+}
