183cbe4d1bf9fcd16b5b7a35acce1f36e8ba47ac
Matt Beckett <beck24@gmail.com>
2012-06-15 11:20:58 -0600

    added content-aware add-content link

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ AU-Bookmarks-Widget-1.8.x
 =========================
 
 Allows additional configuration for the bookmarks widget in Elgg 1.8
+
+Plugin should reside in mod/au_bookmarks_wigdet
--- views/default/widgets/bookmarks/content.php
+++ views/default/widgets/bookmarks/content.php
@@ -2,6 +2,21 @@
 
 $widget = $vars['entity'];
 
+// add in a create content link in certain contexts
+$owner = elgg_get_page_owner_entity();
+if(elgg_get_logged_in_user_guid() == $owner->guid || (elgg_instanceof($owner, 'group') && $owner->isMember())){
+	
+	$url = 'bookmarks/add/' . $owner->guid;
+
+	echo '<div class="au_widgets_add_content">';
+	echo elgg_view('output/url', array(
+		'href' => $url,
+		'text' => elgg_echo('bookmarks:add'),
+		'is_trusted' => TRUE,
+	));
+	echo '</div>';
+}
+
 // now we can get our content
 $options = eligo_get_display_entities_options($widget);
 $content = elgg_list_entities($options);
