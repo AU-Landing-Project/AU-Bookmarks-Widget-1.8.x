2ab00d6c6ca48f4e216f1cefb79468fe875a142f
Matt Beckett <beck24@gmail.com>
2013-01-21 12:41:57 -0700

    removes 'all' context keyword

--- start.php
+++ start.php
@@ -2,7 +2,7 @@
 
 function au_bookmarks_widget_init() {
   elgg_unregister_widget_type('bookmarks');
-  elgg_register_widget_type('bookmarks', elgg_echo('bookmarks'), elgg_echo('bookmarks:widget:description'), 'all,groups', TRUE);
+  elgg_register_widget_type('bookmarks', elgg_echo('bookmarks'), elgg_echo('bookmarks:widget:description'), 'profile,dashboard,index,groups', TRUE);
 }
 
 elgg_register_event_handler('init', 'system', 'au_bookmarks_widget_init');
